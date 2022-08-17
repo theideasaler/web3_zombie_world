8a372f9a787c621e80c759fd99d2378a980ce112
Neo Liu <n.liu@bettechnology.com>
2022-08-17 22:21:16 +1000

    add transfer related code

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -3,37 +3,65 @@ pragma solidity >=0.8.0 <0.9.0;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
-  uint private levelUpFee = 0.001 ether;
+    uint256 private levelUpFee = 0.001 ether;
 
-  function levelUp(uint _zombieId) external payable {
-    require(msg.value == levelUpFee, "You must pay the fee");
-    zombies[_zombieId].level++;
-  }
-
-  modifier aboveLevel(uint _level, unit _zombieId) {
-    require(zombies[_zombieId].level >= _level, "Zombie is not above level");
-    _;
-  }
-
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId], "Only owner can change name");
-    zombies[_zombieId].name = _newName;
-  }
-
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId], "Only owner can change dna");
-    zombies[_zombieId].dna = _newDna;
-  }
-
-  function getZombiesByOwner(address _owner) external view returns(uint[] memeory) {
-    uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    uint counter = 0;
-    for(uint i = 0; i < zombies.length; i++) {
-        if(zombieToOwner[i] == _owner) {
-            result[counter] = i;
-            counter++;
-        }
+    modifier aboveLevel(uint256 _level, unit _zombieId) {
+        require(
+            zombies[_zombieId].level >= _level,
+            "Zombie is not above level"
+        );
+        _;
     }
-    return result;
-  }
+
+    function withdraw() external onlyOwner {
+        address payable _owner = address(uint160(owner()));
+        _owner.transfer(address(this).balance);
+    }
+
+    function setLevelUpFee(uint256 _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
+    function levelUp(uint256 _zombieId) external payable {
+        require(msg.value == levelUpFee, "You must pay the fee");
+        zombies[_zombieId].level++;
+    }
+
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(2, _zombieId)
+    {
+        require(
+            msg.sender == zombieToOwner[_zombieId],
+            "Only owner can change name"
+        );
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+    {
+        require(
+            msg.sender == zombieToOwner[_zombieId],
+            "Only owner can change dna"
+        );
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memeory)
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
 }
