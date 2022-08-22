cf0aa1dc7147bbb2fd0cf5fda265cb66d12a4f6e
Neo Liu <n.liu@bettechnology.com>
2022-08-22 21:48:55 +1000

    fix compilation error for solidity v0.8.0

--- contracts/ownable.sol
+++ contracts/ownable.sol
@@ -17,7 +17,7 @@ pragma solidity >=0.8.0 <0.9.0;
     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
     * account.
     */
-    constructor() internal {
+    constructor() {
       _owner = msg.sender;
       emit OwnershipTransferred(address(0), _owner);
     }
