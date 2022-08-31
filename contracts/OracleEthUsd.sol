21eab353bf86bc6e3b8f9eb07c8a283f3b6123f9
Neo Liu <n.liu@bettechnology.com>
2022-08-31 23:05:58 +1000

    update smart contract with Oracle

---
+++ contracts/OracleEthUsd.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract OracleEthUsd {
+    AggregatorV3Interface public priceFeed;
+
+constructor() public {
+    priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+  }
+
+  function getLatestPrice() public view returns (int) {
+    (,int price,,,) = priceFeed.latestRoundData();
+    return price;
+  }
+
+  function getDecimals() public view returns(uint8) {
+    uint8 decimals = priceFeed.decimals();
+    return decimals;
+  }
+}
